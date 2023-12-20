c1d4410498d6df21ca69343a34193f1f37a96139
llh4github <61351434+llh4github@users.noreply.github.com>
2023-12-20 18:47:17 +0800

    build: 日志配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,11 @@ repositories {
 dependencies {
     implementation("org.apache.logging.log4j:log4j-api-kotlin:1.3.0")
     // SLF4J API
-    implementation("org.slf4j:slf4j-api:1.7.30")
+//    implementation("org.slf4j:slf4j-api:2.0.9")
     // Logback, a SLF4J implementation
-    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("org.apache.logging.log4j:log4j-core:2.22.0")
+    implementation("org.apache.logging.log4j:log4j-api:2.22.0")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.22.0")
 //    runtimeOnly("com.github.ajalt.clikt:clikt-jvm:4.2.1")
 //    runtimeOnly("com.github.ajalt:clikt:4.2.1")
     implementation("io.swagger.core.v3:swagger-annotations:2.2.19")
