7bd77c616ebac91d52faea0a2951763f5ce502c8
llh4github <61351434+llh4github@users.noreply.github.com>
2023-12-25 10:33:30 +0800

    feat: 命令行模块

---
+++ cli/src/main/kotlin/io/github/llh4github/sw3convert/cli/Sw3.kt
@@ -0,0 +1,19 @@
+package io.github.llh4github.sw3convert.cli
+
+import com.github.ajalt.clikt.core.CliktCommand
+
+/**
+ *
+ *
+ * Created At 2023/12/25 10:17
+ * @author llh
+ */
+class Sw3 : CliktCommand() {
+    override fun run() {
+        TODO("Not yet implemented")
+    }
+}
+
+fun main(args: Array<String>) {
+    Sw3().main(args)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "sw3-convert"
-include("core")
+include("core", "cli")
