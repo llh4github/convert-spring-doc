0a9ed00cb3546ded3dbe46100c3bedcdd16a6588
llh4github <61351434+llh4github@users.noreply.github.com>
2023-12-27 11:40:38 +0800

    build: graalvm打包配置

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -5,10 +5,12 @@ version = property("version")!!
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.21"
+    id("org.graalvm.buildtools.native") version "0.9.28"
     application
 }
 repositories {
     mavenCentral()
+    gradlePluginPortal()
 }
 dependencies {
     implementation(project(":core"))
@@ -29,4 +31,19 @@ tasks.withType<Jar> {
         attributes["Main-Class"] = application.mainClass
     }
 }
-
+graalvmNative {
+    toolchainDetection.set(true)
+    binaries {
+        named("main") {
+            imageName.set(application.applicationName)
+            mainClass.set(application.mainClass)
+            buildArgs.add("-O4")
+        }
+        named("test") {
+            buildArgs.add("-O0")
+        }
+    }
+    binaries.all {
+        buildArgs.add("--verbose")
+    }
+}
